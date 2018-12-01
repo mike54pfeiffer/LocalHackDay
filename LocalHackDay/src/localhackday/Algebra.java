9aea9e9c579d31d061c12593badca8fdf48125f1
mike54pfeiffer <32201579+mike54pfeiffer@users.noreply.github.com>
2018-12-01 14:42:58 -0800

    Revert "change"
    This reverts commit a94af0e6560cd245e6df461d008cd5c57611bf83.

--- LocalHackDay/src/localhackday/Algebra.java
+++ LocalHackDay/src/localhackday/Algebra.java
@@ -11,85 +11,8 @@ package localhackday;
  */
 import java.util.*;
 public class Algebra {
-    public void test(String[] args){
+    public static void main(String[] args){
     int problem = (int)((Math.random()*25)+1);
-    if (problem == 1){
-    System.out.print("Find ");
-    } 
-    if (problem == 2){
-    
-    }
-    if (problem == 3){
-    
-    }
-    if (problem == 3){
-    
-    }
-    if (problem == 4){
-    
-    }
-    if (problem == 5){
-    
-    }
-    if (problem == 6){
-    
-    }
-    if (problem == 7){
-    
-    }
-    if (problem == 8){
-    
-    }
-    if (problem == 9){
-    
-    }
-    if (problem == 10){
-    
-    }
-    if (problem == 11){
-    
-    }
-    if (problem == 12){
-    
-    }
-    if (problem == 13){
-    
-    }
-    if (problem == 14){
-    
-    }
-    if (problem == 15){
-    
-    }
-    if (problem == 16){
-    
-    }
-    if (problem == 17){
-    
-    }
-    if (problem == 18){
-    
-    }
-    if (problem == 19){
-    
-    }
-    if (problem == 20){
-    
-    }
-    if (problem == 21){
-    
-    }
-    if (problem == 22){
-    
-    }
-    if (problem == 23){
-    
-    }
-    if (problem == 24){
-    
-    }
-    if (problem == 25){
-    
-    }
+    System.out.print(problem);
     }
 }
