13bac92c7cf1992ec79a650ebd626f595e5e6bc5
mike54pfeiffer <32201579+mike54pfeiffer@users.noreply.github.com>
2018-12-01 11:30:56 -0800

    first commit

---
+++ LocalHackDay/src/localhackday/LocalHackDay.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package localhackday;
+
+/**
+ *
+ * @author Admin
+ */
+public class LocalHackDay {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String test = "Hello World";
+        System.out.print(test);
+    }
+    
+}
