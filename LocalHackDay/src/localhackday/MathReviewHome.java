cb142c50aeef908b52dde3a671e3c7afd3162bc1
Fausto Vidrio <fvidrio76@gmail.com>
2018-12-01 14:58:13 -0800

    Revert "push"
    This reverts commit 4b85dfec2f2ed2f67b5f8a7dfade494c1ab682bb.

--- LocalHackDay/src/localhackday/MathReviewHome.java
+++ LocalHackDay/src/localhackday/MathReviewHome.java
@@ -9,53 +9,6 @@ package localhackday;
  *
  * @author Admin
  */
-
-import java.awt.event.ActionEvent;
-import javax.swing.*;
-import java.util.*;
-import java.awt.event.ActionListener;
-
-public class MathReviewHome extends JFrame implements ActionListener
-{
-
-
-JPanel homePagePanel = new JPanel();
-JLabel headerLabel = new JLabel("Math review questions");
-JLabel promptLabel = new JLabel("Please select a math subject down below");
-String [] mathSubjectList = {"Algebra", "Calculus", "Geometry", "Pre-Algebra", "Pre-Calculus" , "Trigonometry"};
-JComboBox mathSubjectComboBox = new JComboBox(mathSubjectList);
-JButton goButton = new JButton("Go!");
-JButton closeButton = new JButton("Close App");
-
-
-public static void main(String [] args)
-{
-MathReviewHome();
-}
-
- public  MathReviewHome()
-{
-    homePagePanel.add(headerLabel);
-    homePagePanel.add(promptLabel);
-    homePagePanel.add(mathSubjectComboBox);
-    homePagePanel.add(goButton);
-    homePagePanel.add(closeButton);
-    addActionListeners();
-}
+public class MathReviewHome {
     
- public void addActionListeners()
- {
-     goButton.addActionListener(this);
-     closeButton.addActionListener(this);
- }
-
-    @Override
-    public void actionPerformed(ActionEvent e) 
-    {
-  
-    }
-     
-     
- }
- 
-
+}
