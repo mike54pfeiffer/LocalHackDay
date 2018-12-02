cad740bb2617ccf67985dc829542077aa0ab5f83
elephanton <32201130+elephanton@users.noreply.github.com>
2018-12-01 17:31:14 -0800

    commit

--- LocalHackDay/src/localhackday/Calc.java
+++ LocalHackDay/src/localhackday/Calc.java
@@ -5,11 +5,142 @@
  */
 package localhackday;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
  */
 public class Calc {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        System.out.println("If you know any better don't do calc, you're gonna wanna shoot yourself lmao");
+        System.out.println("Press " + "\"y\"" + " if you want to continue, " + "\"n\"" + " if you want to quit.");
+        boolean input = false;
+        do{
+        String response = in.nextLine();
+        if(response.equals("y")){
+            boolean correctAnswer = false;
+            input = true;
+            String answer = null;
+            int counter = 1;
+            System.out.println("You have 3 tries for each question, after 3 tries, the solution will be shown.");
+            System.out.println("Question 1: What is f'(x) when f(x) = 2x^2?");
+            do{
+            response = in.nextLine();
+            if(response.equals("4x")){
+                correctAnswer = true;
+                System.out.println("Correct!");
+             
+            }
+            else {
+                System.out.println("Incorrect, try again.");
+                if(counter == 3){
+                    answer = "4x";
+                    System.out.println("The correct answer is: " + answer + ".");
+                    correctAnswer = true;
+                }
+                counter++;
+            }
+            }
+            while(!correctAnswer);
+            counter = 1;
+            correctAnswer = false;
+            System.out.println("Question 2: Solve ∫ 40x^3 + 12x^2 dx.");
+            do{
+            response = in.nextLine();
+            if(response.equals("10x^4 + 4x^3 + c")){
+                correctAnswer = true;
+                System.out.println("Correct!");
+             
+            }
+            else {
+                System.out.println("Incorrect, try again.");
+                if(counter == 3){
+                    answer = "10x^4 + 4x^3 + c";
+                    System.out.println("The correct answer is: " + answer + ".");
+                    correctAnswer = true;
+                }
+                counter++;
+            }
+            }
+            while(!correctAnswer);
+            counter = 1;
+            correctAnswer = false;
+            System.out.println("Question 3: What is lim x→2 (8−3x+12x2)?");
+            do{
+            response = in.nextLine();
+            if(response.equals("50")){
+                correctAnswer = true;
+                System.out.println("Correct!");
+             
+            }
+            else {
+                System.out.println("Incorrect, try again.");
+                if(counter == 3){
+                    answer = "50";
+                    System.out.println("The correct answer is: " + answer + ".");
+                    correctAnswer = true;
+                }
+                counter++;
+            }
+            }
+            while(!correctAnswer);
+            counter = 1;
+            correctAnswer = false;
+            System.out.println("Question 4: Determine the average value of f(x) = 8x − 3 + 5e^(2−x) on [0,2]");
+            do{
+            response = in.nextLine();
+            if(response.equals("1/2*(5+5e^2)")){
+                correctAnswer = true;
+                System.out.println("Correct!");
+             
+            }
+            else {
+                System.out.println("Incorrect, try again.");
+                if(counter == 3){
+                    answer = "1/2*(5+5e^2)";
+                    System.out.println("The correct answer is: " + answer + ".");
+                    correctAnswer = true;
+                }
+                counter++;
+            }
+            }
+            while(!correctAnswer);
+            counter = 1;
+            correctAnswer = false;
+            System.out.println("Question 5: Determine the area below f(x) = 3 + 2x − x^2 and above the x-axis. ");
+            do{
+            response = in.nextLine();
+            if(response.equals("32/3")){
+                correctAnswer = true;
+                System.out.println("Correct!");
+             
+            }
+            else {
+                System.out.println("Incorrect, try again.");
+                if(counter == 3){
+                    answer = "32/3";
+                    System.out.println("The correct answer is: " + answer + ".");
+                    correctAnswer = true;
+                }
+                counter++;
+            }
+            }
+            while(!correctAnswer);
+        }
+        else if(response.equals("n")) {
+            //code to send back to menu
+            input = true;
+        }
+        else {
+            System.out.println("Please enter a valid response.");
+            
+        }
+     }    
+     while(!input);   
+    }
 
 
     
